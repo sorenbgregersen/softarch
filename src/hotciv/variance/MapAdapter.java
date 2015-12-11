5339d61f867034054f3149610355b0bdb7d02b05
sorenbgregersen <sorenbgregersen@gmail.com>
2015-12-11 11:29:03 +0100

    Again...

--- src/hotciv/variance/MapAdapter.java
+++ src/hotciv/variance/MapAdapter.java
@@ -8,4 +8,28 @@ import thirdparty.ThirdPartyFractalGenerator;
 
 import java.util.HashMap;
 
+public class MapAdapter implements WorldMapStrategy {
+    private ThirdPartyFractalGenerator generator;
+    private StringToTilemap stringToTilemap;
 
+    public MapAdapter() {
+        generator = new ThirdPartyFractalGenerator();
+        stringToTilemap = new StringToTilemap();
+    }
+
+    @Override
+    public HashMap<Position, TileImpl> createWorldMap() {
+        ThirdPartyFractalGenerator generator =
+                new ThirdPartyFractalGenerator();
+        String line;
+        String[] layout = new String[16];
+        for (int r = 0; r < 16; r++) {
+            line = "";
+            for (int c = 0; c < 16; c++) {
+                line = line + generator.getLandscapeAt(r, c);
+            }
+            layout[r] = line;
+        }
+        return stringToTilemap.defineWorld(layout);
+    }
+}
